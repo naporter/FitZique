a161f9a62ea686ea54715713cc785ddc9716e0d7
DrSpectator <51059723+DrSpectator@users.noreply.github.com>
2020-09-21 18:36:16 -0500

    Created LegFragment class and XML. You can now navigate to list of leg work outs.

--- app/src/main/java/com/example/workoutapp/ui/home/LegsFragment.java
+++ app/src/main/java/com/example/workoutapp/ui/home/LegsFragment.java
@@ -2,7 +2,11 @@ package com.example.workoutapp.ui.home;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,15 +15,15 @@ import android.view.ViewGroup;
 import com.example.workoutapp.R;
 
 
-public class LegsFragment extends Fragment {
+public class LegsFragment extends Fragment implements View.OnClickListener{
+
+    NavController navController = null;
 
     public LegsFragment() {
-        // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
-    public static LegsFragment newInstance(String param1, String param2) {
-        LegsFragment fragment = new LegsFragment();
+    public static ArmsFragment newInstance(String param1, String param2) {
+        ArmsFragment fragment = new ArmsFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
@@ -31,9 +35,28 @@ public class LegsFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        view.findViewById(R.id.frontSquatsBtn).setOnClickListener(this);
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_legs, container, false);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.frontSquatsBtn:
+                navController.navigate(R.id.action_legsFragment_to_exerciseFragment);
+                break;
+            default:
+                break;
+        }
     }
 }
