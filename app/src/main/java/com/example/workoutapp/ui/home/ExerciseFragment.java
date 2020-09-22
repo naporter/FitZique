103746d052fcbdab2649cf4d6adfaa3d6c5e8d0a
DrSpectator <51059723+DrSpectator@users.noreply.github.com>
2020-09-22 14:39:47 -0500

    Figure out how to change the set text for each fragment

--- app/src/main/java/com/example/workoutapp/ui/home/ExerciseFragment.java
+++ app/src/main/java/com/example/workoutapp/ui/home/ExerciseFragment.java
@@ -1,22 +1,21 @@
 package com.example.workoutapp.ui.home;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.workoutapp.R;
 
+import static com.example.workoutapp.ui.home.LegsFragment.button;
+
+import org.w3c.dom.Text;
+
 
 public class ExerciseFragment extends Fragment{
-
 
     public ExerciseFragment() {
         // Required empty public constructor
@@ -34,12 +33,29 @@ public class ExerciseFragment extends Fragment{
         super.onCreate(savedInstanceState);
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_exercise, container, false);
+        LayoutInflater lf = getActivity().getLayoutInflater();
+        View view =  lf.inflate(R.layout.fragment_exercise, container, false); //pass the correct layout name for the fragment
+
+        TextView text = (TextView) view.findViewById(R.id.descriptionText);
+        switch (button){
+            case "frontSquatsBtn":
+                button = "frontSquatsBtn";
+                text.setText("Set a barbell on a power rack at about shoulder height. Grab the " +
+                        "power with an overhand grip at shoulder width and raise your elbows until " +
+                        "your upper arms are parallel to the floor. Take the bar out of the rack " +
+                        "and let it rest on your fingertips. Your elbows should be all the way up " +
+                        "throughout the movement. Step back and set your feet at shoulder width " +
+                        "with toes turned out slightly. Squat as low as you can without losing " +
+                        "the arch in your lower back.");
+                break;
+            default:
+                break;
+        }
+        return view;
     }
 
 }
