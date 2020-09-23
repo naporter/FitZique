e08addb044012670f5384b988cba22a0ca7ade5c
Nathan <nathanporter@outlook.com>
2020-09-22 19:10:47 -0500

    Added capability to use ViewModel for ChestFragment and ExerciseFragment.

--- app/src/main/java/com/example/workoutapp/ui/home/ExerciseFragment.java
+++ app/src/main/java/com/example/workoutapp/ui/home/ExerciseFragment.java
@@ -1,28 +1,38 @@
 package com.example.workoutapp.ui.home;
 
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.workoutapp.R;
 
 
-public class ExerciseFragment extends Fragment{
+public class ExerciseFragment extends Fragment {
 
+    private ImageView imageView;
+    private TextView descriptionText;
+    private HomeViewModel homeViewModel;
 
     public ExerciseFragment() {
         // Required empty public constructor
     }
 
-    public static ExerciseFragment newInstance(String param1, String param2) {
+    public static ExerciseFragment newInstance() {
         ExerciseFragment fragment = new ExerciseFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
@@ -34,12 +44,29 @@ public class ExerciseFragment extends Fragment{
         super.onCreate(savedInstanceState);
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_exercise, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        homeViewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                descriptionText.setText(s);
+            }
+        });
+        homeViewModel.getImage().observe(getViewLifecycleOwner(), new Observer<Drawable>() {
+            @Override
+            public void onChanged(Drawable drawable) {
+                imageView.setBackground(drawable);
+            }
+        });
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_exercise, container, false);
+        imageView = view.findViewById(R.id.imageView);
+        descriptionText = view.findViewById(R.id.descriptionText);
+        return view;
+    }
 }
